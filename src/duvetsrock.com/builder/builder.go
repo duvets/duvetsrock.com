0ab13b85a66853e563a9e0ae74d4e53a3e349ad3
Kor <kor@duvetsrock.com>
2014-06-22 17:27:31 +0100

    Initial import of duvetsrock.com

--- src/duvetsrock.com/builder/builder.go
+++ src/duvetsrock.com/builder/builder.go
@@ -3,12 +3,13 @@ package main
 import "encoding/json"
 import "fmt"
 import "io/ioutil"
-import "log"
+import "net/http"
 import "os"
 import "path"
+import "regexp"
 import "time"
 import "html/template"
-import "gopkg.in/yaml.v1"
+import "github.com/go-yaml/yaml"
 
 type BandData struct {
 	Gigs      []Gig
@@ -17,7 +18,7 @@ type BandData struct {
 }
 type Gig struct {
 	StartTime time.Time
-	Duration  string
+	Duration  time.Duration
 	Location  string
 }
 type Location struct {
@@ -33,6 +34,45 @@ type Song struct {
 	Year   int
 }
 
+func get(mapAny interface{}, key string) string {
+	m := mapAny.(map[interface{}]interface{})
+	result := m[key]
+	if result == nil {
+		return ""
+	}
+	return result.(string)
+
+}
+
+var timeRegex = regexp.MustCompile(`(\d\d\d\d-\d\d-\d\d \d\d:\d\d:(?:\d\d)?) ([\w/]+)`)
+
+func parseTime(str string) (time.Time, error) {
+	m := timeRegex.FindStringSubmatch(str)
+	if m != nil {
+		location, err := time.LoadLocation(m[2])
+		if err != nil {
+			return time.Time{}, err
+		}
+		return time.ParseInLocation("2006-01-02 15:04:05", m[1], location)
+	}
+	return time.Time{}, fmt.Errorf("Unable to parse time %q", str)
+}
+
+func (g *Gig) SetYAML(tag string, value interface{}) bool {
+	startTime, err := parseTime(get(value, "startTime"))
+	if err != nil {
+		//fmt.Println(err)
+	}
+	duration, err := time.ParseDuration(get(value, "duration"))
+	if err != nil {
+		//fmt.Printf("Error setting duration %q %v\n", get(value, "duration"), err)
+	}
+	g.StartTime = startTime
+	g.Duration = duration
+	g.Location = get(value, "location")
+	return true
+}
+
 func decodeYAML(path string, result interface{}) error {
 	contents, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -42,7 +82,6 @@ func decodeYAML(path string, result interface{}) error {
 	if err != nil {
 		return fmt.Errorf("Error parsing YAML file %q: %v", path, err)
 	}
-	fmt.Printf("%v", result)
 	return nil
 }
 
@@ -55,39 +94,81 @@ func decodeJSON(path string, result interface{}) error {
 	if err != nil {
 		return fmt.Errorf("Error parsing JSON file %q: %v", path, err)
 	}
-	fmt.Printf("%v", result)
 	return nil
 }
 
-func buildWebsite() error {
-	root := os.Getenv("GOPATH")
-	var err error
+var funcMap = template.FuncMap{
+	"formatTime": func(formatString string, t time.Time) string {
+		return t.Format(formatString)
+	},
+}
 
-	//	s := []Song{}
-	//	err = decodeYAML("/tmp/foo.txt", &s)
-	//	return err
+type config struct {
+	root string
+}
 
-	//s := BandData{}
-	//err = decodeJSON("/tmp/foo.txt", &s)
-	//return err
+var errNotExist = fmt.Errorf("File does not exist")
 
+func writePageContent(config config, filePath string, w http.ResponseWriter) error {
+	fullPath := path.Join(config.root, filePath, "web")
+	if stat, err := os.Stat(fullPath); os.IsNotExist(err) || stat.IsDir() {
+		fullPath = path.Join(fullPath, "index.html")
+	}
+	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+		return errNotExist
+	}
+
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	bandData := BandData{}
-	err = decodeYAML(path.Join(root, "data/data.yaml"), &bandData)
+	err := decodeYAML(path.Join(config.root, "data/data.yaml"), &bandData)
+	if err != nil {
+		return err
+	}
+
+	templateStr, err := ioutil.ReadFile(fullPath)
+	if err != nil {
+		return err
+	}
+	t, err := template.New(filePath).Funcs(funcMap).Parse(string(templateStr))
+	if err != nil {
+		return err
+	}
+	err = t.Execute(w, bandData)
 	if err != nil {
 		return err
 	}
 	return nil
-	t := template.Must(template.ParseFiles(
-		path.Join(root, "web/index.html")))
-	err = t.Execute(os.Stdout, bandData)
-	if err != nil {
-		log.Fatalf("Bad template failed: %s", err)
+}
+
+func serveWebsite(config config) error {
+	fileServer := http.FileServer(http.Dir(path.Join(config.root, "web")))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		err := writePageContent(config, req.URL.Path, w)
+		if err == errNotExist {
+			fileServer.ServeHTTP(w, req)
+			return
+		}
+		w.Header().Add("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("%v", err)))
+	})
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
 	}
-	return nil
+	return s.ListenAndServe()
 }
 
 func main() {
-	err := buildWebsite()
+	config := config{
+		root: os.Getenv("GOPATH"),
+	}
+
+	err := serveWebsite(config)
 	if err != nil {
 		fmt.Printf("%s\n", err)
 		os.Exit(1)
